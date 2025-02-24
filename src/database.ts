4c1d50a1757f3b49919a079743950c6c5cfe19ed
Luiz Claudio <luiz.claudiolc@outlook.com>
2025-02-24 20:06:29 -0300

    chore: configura conexão com BD

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 
 const env = {
-  MONGO_URI: 'mongodb://root:example@127.0.0.1:27021/oz-tech-test?authSource=admin',
+  MONGO_URI: 'mongodb://root:123456@127.0.0.1:27021/oz-map?authSource=admin',
 };
 
 const init = async function() {
-  await mongoose.connect(env.MONGO_URI);
+  await mongoose.connect(env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch(err => console.error('MongoDB connection error:', err));
 };
 
 export default init();
